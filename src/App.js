23687ea998048d39b29a3937bf48f87903b90c29
audgeviolin07 <123830780+audgeviolin07@users.noreply.github.com>
2023-09-16 14:54:31 -0400

    janky frontend

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ChatInterface from './chatinterface';
+import { SidebarScreen } from './AnimaPackage-React-Sidebar/src/screens/SidebarScreen/SidebarScreen';
+import { Slide } from './AnimaPackage-React-Everything/src/screens/Slide/Slide';
 import './App.css';
 
 function App() {
@@ -9,7 +11,9 @@ function App() {
         <h1>IncluScript.ai</h1>
       </header>
       <main className="App-main">
+       
         <ChatInterface />
+        <Slide />
       </main>
     </div>
   );
