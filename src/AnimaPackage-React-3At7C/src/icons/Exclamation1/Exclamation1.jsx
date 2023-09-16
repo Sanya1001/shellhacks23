2d6f4ddfa4ab4320f36e297baee07d87163cb919
audgeviolin07 <123830780+audgeviolin07@users.noreply.github.com>
2023-09-16 11:39:13 -0400

    gptsum add

---
+++ src/AnimaPackage-React-3At7C/src/icons/Exclamation1/Exclamation1.jsx
@@ -0,0 +1,28 @@
+/*
+We're constantly improving the code you see. 
+Please share your feedback here: https://form.asana.com/?k=uvp-HPgd3_hyoXRBw1IcNg&d=1152665201300829
+*/
+
+import React from "react";
+
+export const Exclamation1 = ({ className }) => {
+  return (
+    <svg
+      className={`exclamation-1 ${className}`}
+      fill="none"
+      height="24"
+      viewBox="0 0 25 24"
+      width="25"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        className="path"
+        d="M12.3334 8.99999V11M12.3334 15H12.3434M5.40542 19H19.2614C20.8014 19 21.7634 17.333 20.9934 16L14.0654 3.99999C13.2954 2.66699 11.3714 2.66699 10.6014 3.99999L3.67342 16C2.90342 17.333 3.86542 19 5.40542 19Z"
+        stroke="#919191"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+      />
+    </svg>
+  );
+};
